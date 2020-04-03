38bb0180e06dc46934ed7265a29c95e0a3acaf5a
cortezle <00029117@uca.edu.sv>
2020-04-02 22:14:47 -0600

    first commit

---
+++ src/main/java/com/uca/capas/Laboratorio02SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.uca.capas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio02SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratorio02SpringBootApplication.class, args);
+	}
+
+}
